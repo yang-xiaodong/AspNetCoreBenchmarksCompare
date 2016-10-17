b37d1cab575ae2613bef56d6620db9016e3fbbba
Jay.M.Hu <hm910705@163.com>
2016-10-17 10:59:15 +0800

    Use cluster mode.

--- NodeJSWebApp/HelloWorld.js
+++ NodeJSWebApp/HelloWorld.js
@@ -1,8 +1,21 @@
-var http = require('http');
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-    res.end('Hello World!\n'); 
-}).listen(1337, "127.0.0.1"); 
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-console.log('Server running at http://127.0.0.1:1337/');
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!\n');
+  }).listen(1337, "127.0.0.1");
+  console.log('Server running at http://127.0.0.1:1337/');
+}
